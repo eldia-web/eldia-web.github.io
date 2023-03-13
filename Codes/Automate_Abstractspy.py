0a1128ec4b7d241e076bb6c43ec74b3a024df4a4
eldia-web <125165898+eldia-web@users.noreply.github.com>
2023-03-12 20:44:33 -0700

    !

--- Codes/Automate_Abstractspy.py
+++ Codes/Automate_Abstractspy.py
@@ -5,13 +5,13 @@ import numpy as np
 #%%
 text_or = '<tr><td class="{}" nowrap>{} {}</td><td class="{}">{}</td><td class="{}"><a href="./Abstracts/{}.html">{}</a></td></tr>'
 text_po = '<tr><td class="{}" nowrap>{} {}</td><td class="{}"><a href="./Abstracts/{}.html">{}</a></td></tr>'
-with open(r'C:\Users\Reza\Desktop\ElDia2022.github.io\Codes\text2.txt') as f:
+with open(r'/Users/yhwang/Desktop/HPC_DownloadTemp/eldia-web.github.io/Codes/text2.txt') as f:
     poster = f.readlines()
-with open(r'C:\Users\Reza\Desktop\ElDia2022.github.io\Codes\text_oral.txt') as f:
+with open(r'/Users/yhwang/Desktop/HPC_DownloadTemp/eldia-web.github.io/Codes/text_oral.txt') as f:
     oral = f.readlines()
     
 #%% read excel file
-filename = r'C:\Users\Reza\Desktop\ElDia2022.github.io\Abstracts\Website Presentation Database.xlsx'
+filename = r'/Users/yhwang/Desktop/HPC_DownloadTemp/eldia-web.github.io/Abstracts/Website-Presentation-Database-BAK_2023-SpringBreak.xlsx'
 data = pd.read_excel(filename).set_index('PID')
 
 #%% abstract table
@@ -60,29 +60,29 @@ for j, session in enumerate(sessions):
 poster[70] = poster[70].format(tables[3])
 poster[81] = poster[81].format(tables[4])
 
-with open(r'C:\Users\Reza\Desktop\ElDia2022.github.io\abstracts_poster.html', 'w', encoding='utf-8') as f:
+with open(r'/Users/yhwang/Desktop/HPC_DownloadTemp/eldia-web.github.io/abstracts_poster.html', 'w', encoding='utf-8') as f:
         f.writelines(poster)
 
 oral[73] = oral[73].format(tables[0])
 oral[85] = oral[85].format(tables[1])
 oral[97] = oral[97].format(tables[2])
 
-with open(r'C:\Users\Reza\Desktop\ElDia2022.github.io\abstracts_oral.html', 'w', encoding='utf-8') as f:
+with open(r'/Users/yhwang/Desktop/HPC_DownloadTemp/eldia-web.github.io/abstracts_oral.html', 'w', encoding='utf-8') as f:
         f.writelines(oral)
         
 #%% abstract details
-with open(r'C:\Users\Reza\Desktop\ElDia2022.github.io\Codes\abstract_oral.txt') as f:
+with open(r'/Users/yhwang/Desktop/HPC_DownloadTemp/eldia-web.github.io/Codes/abstract_oral.txt') as f:
     text_oral = f.readlines()
 
-with open(r'C:\Users\Reza\Desktop\ElDia2022.github.io\Codes\abstract_poster.txt') as f:
+with open(r'/Users/yhwang/Desktop/HPC_DownloadTemp/eldia-web.github.io/Codes/abstract_poster.txt') as f:
     text_poster = f.readlines()
 
 #%% write files
 adict = {
-    'Oral Session 3': 'Oral Session 3:  Aerosols, Isotopes, and Soils', 
+    'Oral Session 3': 'Oral Session 3', 
     'Poster Session 2': 'Poster Session 2', 
-    'Oral Session 2': 'Oral Session 2: Data-Driven and Physically-Based Modeling',
-    'Oral Session 1': 'Oral Session 1: Weather and Hydroclimate Extremes', 
+    'Oral Session 2': 'Oral Session 2',
+    'Oral Session 1': 'Oral Session 1', 
     'Poster Session 1': 'Poster Session 1'
     }
 for i in range(0, data.shape[0]):
@@ -91,42 +91,29 @@ for i in range(0, data.shape[0]):
     
     if data.iloc[i]['Presentation Session'] in ['Oral Session 3', 'Oral Session 2', 'Oral Session 1']:
         sample = text_oral.copy()
-        sample[34] = sample[34].format(session, adict[data.iloc[i]['Presentation Session']])
-        sample[36] = sample[36].format(data.iloc[i]['Abstract Text'])
+        sample[34+1] = sample[34+1].format(session, adict[data.iloc[i]['Presentation Session']])
+        sample[36+1] = sample[36+1].format(data.iloc[i]['Abstract Text'])
     elif data.iloc[i] ['Presentation Session'] in ['Poster Session 1', 'Poster Session 2']:
         sample = text_poster.copy()
-        sample[35] = sample[35].format(session, adict[data.iloc[i]['Presentation Session']])
-        sample[37] = sample[37].format(data.iloc[i]['Abstract Text'])
+        sample[35+1] = sample[35+1].format(session, adict[data.iloc[i]['Presentation Session']])
+        sample[37+1] = sample[37+1].format(data.iloc[i]['Abstract Text'])
         if str(data.iloc[i]['Link to PDF']) == 'nan': 
-            sample[34] = sample[34].format("")
+            sample[34+1] = sample[34+1].format("")
         else:
-            sample[34] = sample[34].format(data.iloc[i]['Link to PDF'])
+            sample[34+1] = sample[34+1].format(data.iloc[i]['Link to PDF'])
         
     sample[3] = sample[3].format(data.iloc[i]['First Name'])
-    sample[25] = sample[25].format(data.iloc[i]['Abstract Title'].title())
-    sample[27] = sample[27].format(data.iloc[i]['First Name'], data.iloc[i]['Last Name'])
-    sample[28] = sample[28].format(coauthors)
-    sample[29] = sample[29].format(data.iloc[i]['Faculty Advisors'])
-    sample[30] = sample[30].format(data.iloc[i]['Affiliation'])
+    sample[25+1] = sample[25+1].format(data.iloc[i]['Abstract Title'].title())
+    sample[27+1] = sample[27+1].format(data.iloc[i]['First Name'], data.iloc[i]['Last Name'])
+    sample[28+1] = sample[28+1].format(coauthors)
+    sample[29+1] = sample[29+1].format(data.iloc[i]['Faculty Advisors'])
+    sample[30+1] = sample[30+1].format(data.iloc[i]['Affiliation'])
     
     if str(data.iloc[i]['Link to Video']) == 'nan': 
-        sample[33] = sample[33].format("")
+        sample[33+1] = sample[33+1].format("")
     else:
-        sample[33] = sample[33].format(data.iloc[i]['Link to Video'])
+        sample[33+1] = sample[33+1].format(data.iloc[i]['Link to Video'])
     
-    filename = r'C:\Users\Reza\Desktop\ElDia2022.github.io\Abstracts\{}_{}.html'.format(data.index[i], data.iloc[i]['First Name'])
+    filename = r'/Users/yhwang/Desktop/HPC_DownloadTemp/eldia-web.github.io/Abstracts/{}_{}.html'.format(data.index[i], data.iloc[i]['First Name'])
     with open(filename, 'w', encoding='utf-8') as f:
         f.writelines(sample)
-
-
-
-
-
-
-
-
-
-
-
-
-    
